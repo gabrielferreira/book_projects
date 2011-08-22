ba30478d41c53e25abce8886fa843edbc15224d9
Gabriel <gabriel.d3v@gmail.com>
2011-08-22 18:06:38 -0300

    Create Swap

--- Swap/Swap/SwapViewController.h
+++ Swap/Swap/SwapViewController.h
@@ -8,6 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SwapViewController : UIViewController
+#define degressToRadians(x) (M_PI * (x) / 180.0)
+
+@interface SwapViewController : UIViewController {
+    UIView *landscape;
+    UIView *portrait;
+    
+    //Foo
+    UIButton *landscapeFooButton;
+    UIButton *portraitFooButton;
+    
+    //Bar
+    UIButton *landscapeBarButton;
+    UIButton *portraitBarButton;
+}
+
+@property (nonatomic, retain) IBOutlet UIView *landscape;
+@property (nonatomic, retain) IBOutlet UIView *portrait;
+@property (nonatomic, retain) IBOutlet UIView *landscapeFooButton;
+@property (nonatomic, retain) IBOutlet UIView *portraitFooButton;
+@property (nonatomic, retain) IBOutlet UIView *landscapeBarButton;
+@property (nonatomic, retain) IBOutlet UIView *portraitBarButton;
+- (IBAction)buttonPressed:(id)sender;
 
 @end
