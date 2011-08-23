b8f6a720b0ab5c6393998bab88ab54acec04f509
Gabriel <gabriel.d3v@gmail.com>
2011-08-23 16:11:08 -0300

    Finish Swap

--- Swap/Swap/SwapViewController.m
+++ Swap/Swap/SwapViewController.m
@@ -9,6 +9,50 @@
 #import "SwapViewController.h"
 
 @implementation SwapViewController
+@synthesize landscape;
+@synthesize portrait;
+@synthesize landscapeFooButton;
+@synthesize portraitFooButton;
+@synthesize landscapeBarButton;
+@synthesize portraitBarButton;
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    if (toInterfaceOrientation == UIInterfaceOrientationPortrait) {
+        self.view = self.portrait;
+        self.view.transform = CGAffineTransformIdentity;
+        self.view.transform = CGAffineTransformMakeRotation(defressToRadians(0));
+        self.view.bounds = CGRectMake(0.0, 0.0, 320.0, 460.0);
+    }
+    else if (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft) {
+        self.view = self.landscape;
+        self.view.transform = CGAffineTransformIdentity;
+        self.view.transform = CGAffineTransformMakeRotation(defressToRadians(-90));
+        self.view.bounds = CGRectMake(0.0, 0.0, 480.0, 300.0);
+    }
+    else if (toInterfaceOrientation == UIInterfaceOrientationPortraitUpsideDown) {
+        self.view = self.portrait;
+        self.view.transform = CGAffineTransformIdentity;
+        self.view.transform = CGAffineTransformMakeRotation(defressToRadians(180));
+        self.view.bounds = CGRectMake(0.0, 0.0, 320.0, 460.0);
+    }
+    else if (toInterfaceOrientation == UIInterfaceOrientationLandscapeRight) {
+        self.view = self.landscape;
+        self.view.transform = CGAffineTransformIdentity;
+        self.view.transform = CGAffineTransformMakeRotation(defressToRadians(90));
+        self.view.bounds = CGRectMake(0.0, 0.0, 480.0, 300.0);
+    }
+}
+
+- (IBAction)buttonPressed:(id)sender {
+    if (sender == portraitFooButton || sender == landscapeFooButton) {
+        portraitFooButton.hidden = YES;
+        landscapeFooButton.hidden = YES;
+    }
+    else {
+        portraitBarButton.hidden = YES;
+        landscapeBarButton.hidden = YES;
+    }
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -30,15 +74,31 @@
 
 - (void)viewDidUnload
 {
+    self.landscape = nil;
+    self.portrait = nil;
+    self.landscapeFooButton = nil; 
+    self.portraitFooButton = nil; 
+    self.landscapeBarButton = nil;
+    self.portraitBarButton = nil;
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
 
+- (void)dealloc {
+    [landscape release];
+    [portrait release];
+    [landscapeFooButton release];
+    [portraitFooButton release];
+    [landscapeBarButton release];
+    [portraitBarButton release];
+    [super dealloc];
+}
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 @end
